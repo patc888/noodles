bcdc5e6d36f4daf1aac2d444dff13d60cc9ec11d
Patrick Chan <patc888@yahoo.com>
2013-11-04 07:39:12 -0800

    increase threshold

--- apis/wallabee.js
+++ apis/wallabee.js
@@ -88,7 +88,7 @@ findLower = function(request, reply) {
               itemType = obj[key].item_type_id;
               var num = parseInt(obj[key].number);
               var cost = parseInt(obj[key].cost);
-              if (cost <= 1000) {
+              if (cost <= 2000) {
                 if (!items[itemType]) {
                   items[itemType] = { "number": num, "cost": cost };
                 } else if (num < items[itemType].number) {
