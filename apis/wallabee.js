ee0ed1d11d0e0d10372a6e4151bc67c4142e1705
Patrick Chan <patc888@yahoo.com>
2014-09-01 14:45:45 -0700

    updates

--- apis/wallabee.js
+++ apis/wallabee.js
@@ -69,14 +69,14 @@ findRares = function(request, reply) {
                 itemType = obj[key].item_type_id;
                 var num = parseInt(obj[key].number);
                 var cost = parseInt(obj[key].cost);
-                if (cost <= 5000) {
+                //if (cost <= 5000) {
                   if (!items[itemType]) {
                     items[itemType] = { "number": num, "cost": cost };
                   } else if (num < items[itemType].number) {
                     items[itemType].number = num;
                     items[itemType].cost = cost;
                   }
-                }
+                //}
               }
               resolve(items);
             }
